817b6695eec01798c425ed2ac913ea185c5cfc0b
Ming Yan <mingyan214@gmail.com>
2016-06-02 22:49:33 +1000

    form validators

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,22 +1,12 @@
-import {Component} from 'angular2/core'
-import {CoursesComponent} from './courses.component'
-import {AuthorsComponent} from './authors.component'
-import { ZippyComponent } from './zippy.component';
-import { SubscriptionForm } from './subscription-form.component';
+import {Component} from 'angular2/core';
+import {SignUpFormComponent} from './signup-form.component'
 
 @Component({
     selector: 'my-app',
+    directives: [SignUpFormComponent],
     template: `
-    <zippy title="This is a title">
-    <div>Body content here</div>
-    </zippy>
-    <subscription-form></subscription-form>
-    `,
-    directives: [
-        CoursesComponent,
-        AuthorsComponent,
-        ZippyComponent,
-        SubscriptionForm
-        ]
+        <signup-form></signup-form>
+    `
 })
-export class AppComponent { }
+export class AppComponent {
+}
