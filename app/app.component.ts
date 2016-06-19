06009509bd2ff1c0912fd795be3fe30f92e22065
Ming Yan <mingyan214@gmail.com>
2016-06-19 23:15:00 +1000

    playing with routing

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,13 +1,8 @@
 import {Component} from 'angular2/core';
-import {UpdatePasswordFormComponent} from './update-password-form.component'
-import {GithubUserComponent} from './github-user.component';
 
 @Component({
     selector: 'my-app',
-    directives: [GithubUserComponent],
-    template: `
-        <github-user>
-    `
+    templateUrl: '/app/app.component.html'
 })
 export class AppComponent {
 }
